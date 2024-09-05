2019a8595f849b7fa24747e80b0e28a446b33807
Bryan Oliveira <bsoliveira@proton.me>
2024-09-05 17:43:46 -0300

    feat: Faz o GET das locations e monta a arvore inicial

--- src/components/AssetsTree.tsx
+++ src/components/AssetsTree.tsx
@@ -1,33 +1,42 @@
 import { BiChevronDown } from "react-icons/bi";
 import { FiMapPin } from "react-icons/fi";
+import useCompany from "../store/useCompany";
+import { useQuery } from "@tanstack/react-query";
+import { api } from "../lib/api";
+import Asset from "../models/Asset";
+import Location from "../models/Location";
 
 export default function AssetsTree() {
+    const company = useCompany((state) => state.company)
+
+    const { data: locations } = useQuery<{ data: Location[] }>({
+        queryKey: ['locations', company?.id],
+        queryFn: async () => await api.get(`/companies/${company?.id}/locations`),
+    })
+
+    const { data: assets } = useQuery<{ data: Asset[] }>({
+        queryKey: ['assets', company?.id],
+        queryFn: async () => await api.get(`/companies/${company?.id}/assets`)
+    })
+
+    const rootLocations = locations?.data.filter((loc) => !loc.parentId);
+
+    const tree = rootLocations?.map((loc) => ({
+        ...loc,
+        children: locations?.data.filter((l) => l.parentId === loc.id)
+    }))
+
+
     return (
-        <div className="flex flex-col space-y-4 border-t border-gray-300 dark:border-gray-600 p-3">
+        <div className="flex flex-col space-y-4 border-t border-gray-300 dark:border-gray-600 p-3 overflow-auto">
             
-            <div className="flex space-x-2 items-center cursor-pointer">
-                <BiChevronDown className="w-5 h-5"/>
-                <FiMapPin className="text-blue-500 w-5 h-5"/> 
-                <span>PRODUCTION AREA - RAW MATERIAL</span>
-            </div>
-
-            <div className="flex space-x-2 items-center cursor-pointer">
-                <BiChevronDown className="w-5 h-5"/>
-                <FiMapPin className="text-blue-500 w-5 h-5"/> 
-                <span>PRODUCTION AREA - RAW MATERIAL</span>
-            </div>
-
-            <div className="flex space-x-2 items-center cursor-pointer">
-                <BiChevronDown className="w-5 h-5"/>
-                <FiMapPin className="text-blue-500 w-5 h-5"/> 
-                <span>PRODUCTION AREA - RAW MATERIAL</span>
-            </div>
-
-            <div className="flex space-x-2 items-center cursor-pointer">
-                <BiChevronDown className="w-5 h-5"/>
-                <FiMapPin className="text-blue-500 w-5 h-5"/> 
-                <span>PRODUCTION AREA - RAW MATERIAL</span>
-            </div>
+            {tree?.map((location) => (
+                <div className="flex space-x-2 items-center cursor-pointer">
+                    <BiChevronDown className="w-5 h-5"/>
+                    <FiMapPin className="text-blue-500 w-5 h-5"/> 
+                    <span>{ location.name }</span>
+                </div>
+            ))}
             
         </div>
     )
---
+++ src/models/Location.ts
@@ -0,0 +1,5 @@
+export default interface Location {
+    id: string;
+    name: string;
+    parentId: string | null;   
+}
